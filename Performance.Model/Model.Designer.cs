93a52e1871a36b8330c90ab3e5b362682b152d99
Alejandra Echevarrieta <alechevarrieta@gmail.com>
2024-04-22 15:22:26 -0300

    modelo

--- Performance.Model/Model.Designer.cs
+++ Performance.Model/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\lucas\OneDrive\Escritorio\Trabajo\Performance\Performance.Model\Model.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\marti\OneDrive\Escritorio\Performance\Performance.Model\Model.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
