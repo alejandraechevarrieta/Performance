4d737f0ebfe3d3d877da61e0c59e153982cd68bd
Lucas Lecea <lucaslecea2003@gmail.com>
2024-06-03 11:02:03 -0300

    Cambio imagen en mail

--- Performance.Servicios/ServicioPerformance.cs
+++ Performance.Servicios/ServicioPerformance.cs
@@ -260,7 +260,7 @@ namespace Performance.Servicios
                     "<table border=\"0\" width=\"200px\" bgcolor=\"#EDECEB\"> \r\n    " +
                         "<tbody> \r\n        " +
                             "<tr> \r\n            " +
-                                "<td align=\"center\"></td> \r\n        " +
+                                "<td align=\"center\"><img src=\"https://buhogestion.distrocuyo.com/content/perform/performanceIdentidad2024.png\" alt=\"\" width=\"150px\"></td> \r\n        " +
                             "</tr> \r\n    " +
                         "</tbody> \r\n" +
                     "</table>\r\n" +
